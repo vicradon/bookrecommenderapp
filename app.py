f463a27a10d60a578467c7fb7861b699d14d5e79
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-06-05 03:46:44 +0100

    chore: add default host to app run

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def favorite_books():
     return render_template('favorites.html', favorites=favorites)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
